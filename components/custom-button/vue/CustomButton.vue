4d6fc7966705660600767b2603ac5997ad59505e
Natalya Berdnikova <kitakava@mail.ru>
2023-06-22 20:09:40 +0500

    Add button, change style for tabs and wizard

---
+++ components/custom-button/vue/CustomButton.vue
@@ -0,0 +1,20 @@
+<template lang="pug">
+button.custom-button(:class="{ '_secondary': isSecondary }")
+  slot
+</template>
+
+<script>
+export default {
+  name: 'CustomButton',
+  props: {
+    isSecondary: {
+      type: Boolean,
+      default: false
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+@import "../styles/custom-button.scss";
+</style>
--- components/wizard/vue/Wizard.vue
+++ components/wizard/vue/Wizard.vue
@@ -1,17 +1,30 @@
 <template lang="pug">
 .wizard
   Tabs(:activeIndex="activeTabIndex" @change="onChangeActiveIndex")
+  component(:is="activeWizardStep")
+  .wizard__buttons
+    CustomButton(class="wizard__button") Сохранить
+    CustomButton(class="wizard__button" isSecondary) Очистить
 </template>
 
 <script>
 import Tabs from '@/components/tabs/vue/Tabs.vue';
+import CustomButton from '@/components/custom-button/vue/CustomButton.vue';
+
+const STEPS = [];
 
 export default {
   name: "Wizard",
-  components: { Tabs },
+  components: { Tabs, CustomButton },
   data() {
     return {
       activeTabIndex: 0
+    }
+  },
+
+  computed: {
+    activeWizardStep() {
+      return STEPS[this.activeTabIndex];
     }
   },
 
@@ -23,3 +36,7 @@ export default {
   }
 }
 </script>
+
+<style lang="scss" scoped>
+@import "../styles/wizard.scss";
+</style>
